c416f7bff2b3d64b19cf5ae3f5598f2070afa1f6
Masha <yashkamasha590@gmail.com>
2022-06-10 15:28:16 +0300

    compl

---
+++ 18/zad2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<char> symbols = new Queue<char>();
+            Queue<char> digits = new Queue<char>();
+
+
+            using (StreamReader sr = new StreamReader("C:\\Users\\Владислав\\Desktop\\uchebnaya_praktika\\18\\zad18\\zad2\\2.txt"))
+            {
+
+                while (!sr.EndOfStream)
+                {
+
+                    char s = (char)sr.Read();
+                    if (char.IsDigit(s)) digits.Enqueue(s);
+                    else symbols.Enqueue(s);
+
+                }
+
+            }
+
+            Console.WriteLine(string.Join("", symbols) + string.Join("", digits));
+        }
+    }
+}
